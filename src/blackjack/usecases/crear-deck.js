6669b147b5f484499d61726ddd93e8d943388ed6
Jesús Cedeño Vélez <jesus.27velez@gmail.com>
2025-10-27 16:21:18 -0500

    Docs: Add JSDoc documentation and validation to crearDeck function

--- src/blackjack/usecases/crear-deck.js
+++ src/blackjack/usecases/crear-deck.js
@@ -1,7 +1,17 @@
 import _ from 'underscore';
+// export const miNombre = 'Jesús';
 
-// Esta función crea un nuevo deck
+/**
+ * Esta función crea un nuevo deck
+ * @param { Array <String> } tiposDeCarta -> Ejemplo = ['C', 'D', 'H', 'S']
+ * @param { Array <String> } tiposEspeciales -> Ejemplo = ['A', 'J', 'Q', 'K']
+ * @returns { Array <String> } retorna un nuevo deck de cartas
+ */
+
 export const crearDeck = (tiposDeCarta, tiposEspeciales) => {
+
+  if (!tiposDeCarta || tiposDeCarta.length === 0 )
+    throw new Error('tiposDeCarta es obligatorio como un arreglo de string');
 
   let deck = [];
 
@@ -20,3 +30,5 @@ export const crearDeck = (tiposDeCarta, tiposEspeciales) => {
   deck = _.shuffle(deck);
   return deck;
 }
+
+// export default crearDeck;
